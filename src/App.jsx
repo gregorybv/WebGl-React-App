234eb4d81be7c0873ad19efaae87e394ccfacd51
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-02 11:02:55 +0300

    new connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import DisplaySection from "./components/DisplaySection"
 import Jumbotron from "./components/Jumbotron"
 import Nav from "./components/Nav"
 import SoundSection from "./components/SoundSection"
@@ -8,6 +9,7 @@ function App() {
       <Nav />
       <Jumbotron />
       <SoundSection />
+      <DisplaySection />
     </div>
   )
 }
