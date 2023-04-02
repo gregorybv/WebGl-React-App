a6c2b8f98d80d144db4dab37f1719eb843ad60fa
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-02 12:05:16 +0300

    new edits

--- src/components/DisplaySection.jsx
+++ src/components/DisplaySection.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react"
 
 const DisplaySection = () => {
   return (
-    <div>
-      
+    <div className='display-section wrapper'>
+      <h2 className='title'>New</h2>
+      <p className='text'>Brilliant.</p>
+      <span className='description'>
+        A display that's up to 2x brighter in the sun.
+      </span>
+      <button className='button'>Try me!</button>
+      <button className='back-button'>TOP</button>
     </div>
   )
 }
