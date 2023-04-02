c2705660da8a77deedc031dad1690d4b9ba225c0
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-02 14:30:12 +0300

    new finction

--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -16,7 +16,6 @@ import {
   SSAOPlugin,
   BloomPlugin,
   GammaCorrectionPlugin,
-  addBasePlugins,
   mobileAndTabletCheck,
 } from "webgi"
 import { gsap } from "gsap"
@@ -26,48 +25,42 @@ const WebgiViewer = () => {
   const canvasRef = useRef(null)
 
   const setupViewer = useCallback(async () => {
-    {
-      // Initialize the viewer
-      const viewer = new ViewerApp({
-        canvas: canvasRef.current,
-      })
+    const viewer = new ViewerApp({
+      canvas: canvasRef.current,
+    })
 
-      // Add some plugins
-      const manager = await viewer.addPlugin(AssetManagerPlugin)
+    const manager = await viewer.addPlugin(AssetManagerPlugin)
 
-      // Add plugins individually.
-      // await viewer.addPlugin(GBufferPlugin)
-      // await viewer.addPlugin(new ProgressivePlugin(32))
-      // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-      // await viewer.addPlugin(GammaCorrectionPlugin)
-      // await viewer.addPlugin(SSRPlugin)
-      // await viewer.addPlugin(SSAOPlugin)
-      // await viewer.addPlugin(DiamondPlugin)
-      // await viewer.addPlugin(FrameFadePlugin)
-      // await viewer.addPlugin(GLTFAnimationPlugin)
-      // await viewer.addPlugin(GroundPlugin)
-      // await viewer.addPlugin(BloomPlugin)
-      // await viewer.addPlugin(TemporalAAPlugin)
-      // await viewer.addPlugin(AnisotropyPlugin)
+    const camera = viewer.scene.activeCamera
+    const position = camera.position
+    const target = camera.target
 
-      // or use this to add all main ones at once.
-      await addBasePlugins(viewer)
+    await viewer.addPlugin(GBufferPlugin)
+    await viewer.addPlugin(new ProgressivePlugin(32))
+    await viewer.addPlugin(new TonemapPlugin(true))
+    await viewer.addPlugin(GammaCorrectionPlugin)
+    await viewer.addPlugin(SSRPlugin)
+    await viewer.addPlugin(SSAOPlugin)
+    await viewer.addPlugin(BloomPlugin)
 
-      // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
-      await viewer.addPlugin(CanvasSnipperPlugin)
+    viewer.renderer.refreshPipeline()
 
-      // This must be called once after all plugins are added.
-      viewer.renderer.refreshPipeline()
+    await manager.addFromPath("scene-black.glb")
 
-      await manager.addFromPath("scene-black.glb")
+    viewer.getPlugin(TonemapPlugin).config.clipBackground = true
 
-      // Load an environment map if not set in the glb file
-      // await viewer.scene.setEnvironment(
-      //     await manager.importer!.importSinglePath<ITexture>(
-      //         "./assets/environment.hdr"
-      //     )
-      // );
-    }
+    viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false })
+
+    window.scrollTo(0, 0)
+
+    let needsUpdate = true
+
+    viewer.addEventListener("preFrame", () => {
+      if (needsUpdate) {
+        camera.positionTargetUpdated(true)
+        needsUpdate = false
+      }
+    })
   }, [])
 
   useEffect(() => {
