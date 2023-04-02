4691ef6335d3383037bfbb069497384b59b406a2
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-02 10:30:07 +0300

    new edits

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Logo from '../assets/images/logo.svg'
 
 const Nav = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='nav-wrapper'>
+      <div className='nav-content'>
+        <ul className='list-styled'>
+          <li>
+            <img src={Logo} alt='Apple'/>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
