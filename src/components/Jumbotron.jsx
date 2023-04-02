ed7e60e4de7caf6fada5c7513ac1568d741a68c7
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-02 10:45:45 +0300

    new edits

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react"
+import Iphone from "../assets/images/iphone-14.jpg"
 
 const Jumbotron = () => {
   return (
-    <div>
-      
+    <div className='jumbotron-section wrapper'>
+      <h2 className='title'>New</h2>
+      <img className='logo' src={Iphone} alt='Iphone' />
+      <p className="text">Big and bigger.</p>
+      <span className="description">
+        From $41.62/mo. for 24 mo. or 999$ before trade-in
+      </span>
     </div>
   )
 }
