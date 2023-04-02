cae49ceb258baf65b3294521c5169345b9c896ff
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-02 10:45:38 +0300

    connect component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Jumbotron from "./components/Jumbotron"
 import Nav from "./components/Nav"
 
 function App() {
   return (
     <div className='App'>
       <Nav />
+      <Jumbotron />
     </div>
   )
 }
