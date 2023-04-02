99b16e2976d2ac23dbfc5deb25db313f73c1ee3f
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-02 10:49:08 +0300

    new edits

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,15 +1,25 @@
 import React from "react"
 import Iphone from "../assets/images/iphone-14.jpg"
+import HoldingIphone from "../assets/images/iphone-hand.png"
 
 const Jumbotron = () => {
   return (
     <div className='jumbotron-section wrapper'>
       <h2 className='title'>New</h2>
       <img className='logo' src={Iphone} alt='Iphone' />
-      <p className="text">Big and bigger.</p>
-      <span className="description">
+      <p className='text'>Big and bigger.</p>
+      <span className='description'>
         From $41.62/mo. for 24 mo. or 999$ before trade-in
       </span>
+      <ul className='links'>
+        <li>
+          <button className='button'>Buy</button>
+        </li>
+        <li>
+          <a className='link'>Learn more</a>
+        </li>
+      </ul>
+      <img className='iphone-img' src={HoldingIphone} alt='HoldingIphone' />
     </div>
   )
 }
